b605a27fef66f4e352df4744230314a701136045
Genival SILVA <genival.silva@valeo.com>
2020-04-29 08:46:49 -0300

    add index file

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+//Dicionário
+var frutas = [{nome:"maça", cor:"vermelha", peso:"50 g"},{nome:"uva",cor:"roxa",peso:"10 g"}]
+console.log(frutas);
+console.log(frutas[1].nome);
+console.log(frutas[0].cor);
+console.log(frutas[1].peso);
+
+
+
+
+/*var lista = ["maça","pêra","laranja"];
+//lista.push("uva");
+//lista.pop();
+//
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+
+/*var nome = "Genival Silva";
+var idade = 40;
+var frase = "Japão é o melhor time do Mundo";
+//alert("Welcome " + nome)
+//alert(nome + " has " + idade + " old" )
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())*/
